d9d9308409e42a95ed353f904ae2b0749f5f94ef
kasradara <50513622+kasradara@users.noreply.github.com>
2022-07-15 19:15:50 +0430

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <span>credit : {{credit}}</span>
+    <div class="symbols">
+      <symbols v-for="(item) in result" :key="item" :symbol="item" />
+    </div>
+    <button @click="getResult">spin</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import symbols from '@/components/symbols.vue'
 
 export default {
+  components: {symbols},
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      credit: 10,
+      symbols: [
+        {name: 'cherry', reward: 10, slug: 'c'},
+        {name: 'lemon', reward: 20, slug: 'l'},
+        {name: 'orange', reward: 30, slug: 'o'},
+        {name: 'watermelon', reward: 40, slug: 'w'}
+      ],
+      result: ['x','y','z'],
+      loading : [false , false , false]
+    }
+  },
+  created() {
+    console.log(Math.floor(Math.random()*4))
+  },
+  methods: {
+    async spin() {
+      let symbols = []
+      for (let i = 0; i < 3; i++) {
+        const symbolRandomId = Math.floor(Math.random()*4);
+        symbols.push(this.symbols[symbolRandomId].slug)
+      }
+      return await symbols
+    },
+    getResult() {
+      this.spin().then(res => {
+        // if(res.every(symbol => symbol == res[0]) ){
+        //   console.log('hi')
+        // }
+        this.result[0] = res[0]
+        this.result[1] = res[1]
+        this.result[2] = res[2]
+        this.loading = true
+        console.log(this.result)
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.symbols{
+  display: flex;
+  gap: 20px;
+}
+.symbols .symbol{
+  font-size: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: gray;
+  width: 33%;
 }
 </style>
