23edf5442978de619832c9e64ba465c2f4365d61
kasradara <50513622+kasradara@users.noreply.github.com>
2022-07-16 10:53:14 +0430

    app done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
-    <span>credit : {{credit}}</span>
-    <div class="symbols">
-        <div class="symbol" v-for="(symbol, index) in result" :key="index">
-            <span>{{symbol}}</span>
-        </div>
+  <div class="container">
+    <span class="credit">credit : {{credit}}</span>
+    <table class="symbols">
+        <td v-for="x in 3" :key="x" :class="['symbols-placeholder', {left33:x==2, left66:x==3}]">
+          <span :class="['symbol-charecter', {loading:loading}]">
+            x
+          </span>
+        </td>
+        <td class="symbol" v-for="(symbol, index) in result" :key="Math.random() * index">
+          <span class="symbol-charecter">{{symbol}}</span>
+
+        </td>
+    </table>
+    <div class="buttons" v-if="credit">
+      <button class="btn" :disabled="disableSpin || cashedOut" @click="getResult">spin</button>
+      <button
+        :class="['btn', {cashout: moveCashout}]" 
+        :disabled="disableCashout || cashedOut"
+        @mouseenter="cashOutHover()"
+        @click="cashedOut = true"
+      >
+        cash out
+      </button>
     </div>
-    <button @click="getResult">spin</button>
+    <p class="message">{{message}}</p>
   </div>
 </template>
 
@@ -16,19 +33,37 @@ export default {
   name: 'App',
   data() {
     return {
-      credit: 75,
+      credit: 10,
       symbols: [
-        {name: 'cherry', reward: 10, slug: 'c'},
-        {name: 'lemon', reward: 20, slug: 'l'},
-        {name: 'orange', reward: 30, slug: 'o'},
-        {name: 'watermelon', reward: 40, slug: 'w'}
+        {name: 'cherry', reward: 10, slug: 'C'},
+        {name: 'lemon', reward: 20, slug: 'L'},
+        {name: 'orange', reward: 30, slug: 'O'},
+        {name: 'watermelon', reward: 40, slug: 'W'}
       ],
-      result: ['','',''],
-      winsOnce: false
+      result: [],
+      loading: false,
+      winsOnce: false,
+      disableSpin: false,
+      disableCashout: false,
+      moveCashout: false,
+      cashedOut: false,
     }
   },
-  created() {
-    console.log(Math.floor(Math.random()*4))
+  computed: {
+    message() {
+      if(!this.credit){
+        return 'you have no credit to spin or cashout';
+      }
+      if(this.cashedOut){
+        return `you cashedout ${this.credit} credit`;
+      } 
+      if(this.disableCashout || this.moveCashout) {
+        return 'you cant cashout right now'
+      } 
+      return ''
+
+      
+    }
   },
   methods: {
     async spin() {
@@ -40,8 +75,10 @@ export default {
       return await symbols
     },
     getResult() {
+      this.loading = true
+      this.disableSpin = true
+      this.result = []
       this.spin().then(res => {
-        console.log(res)
         if(res.every(symbol => symbol == res[0]) && !this.winsOnce && this.couldCheat()){
           this.winsOnce = true;
           this.getResult()
@@ -51,36 +88,121 @@ export default {
         res.forEach((element,ind) => {
           setTimeout(()=>{
             this.result.push(element)
-          }, (3+ind)*1000)
+          }, (1+ind)*1000)
         });
-
-        console.log(this.result)
-        this.winsOnce = false
+        setTimeout(()=>{
+          this.setCredit(res)
+          this.winsOnce = false
+          this.disableCashout = false
+          this.disableSpin = false
+          this.moveCashout = false
+        }, 3000)
       })
     },
     couldCheat(){
       let percent = Math.floor(Math.random()*101)
-      console.log(percent)
       if(this.credit >= 40 && this.credit < 60 && percent <= 30) {
         return true
       }
       if(this.credit >= 60 && percent <= 60) {
         return true
       } return false
-    }
+    },
+    setCredit(symbols) {
+      if(symbols.every(symbol => symbol == 'C')){
+        this.credit = this.credit + 10
+      } else if(symbols.every(symbol => symbol == 'L')){
+        this.credit = this.credit + 20
+      } else if(symbols.every(symbol => symbol == 'O')){
+        this.credit = this.credit + 30
+      } else if(symbols.every(symbol => symbol == 'W')){
+        this.credit = this.credit + 40
+      } else this.credit--
+    },
+    cashOutHover() {
+      let percent = Math.floor(Math.random()*101)
+      if(percent <= 40){
+        this.disableCashout = true
+      } else if(percent > 40 && percent <= 90 ) {
+        this.moveCashout = true
+      }
+    },
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0;
+  background-color:antiquewhite
+}
+.container{
+  max-width: 500px;
+  height: 80vh;
+  border-radius: 20px;
+  margin: 20px auto 0;
+  background: brown;
+}
+.credit{
+    text-align: center;
+    display: block;
+    padding-top: 18px;
+    font-size: 20px;
+    font-weight: 900;
+    color: white;
+}
 .symbols{
+  background-color: white;
+  margin-top: 32px;
+  height: 150px;
+  position: relative;
   display: flex;
-  gap: 20px;
 }
 .symbols .symbol{
   font-size: 20px;
+  z-index: 1;
   text-align: center;
-  background: gray;
+  background: white;
   width: 33%;
+}
+.symbol-charecter{
+  font-size: 96px;
+  text-align: center;
+  display: block;
+}
+.symbols-placeholder{
+  width: 33.3%;
+  background: white;
+  position: absolute;
+  height: 148px;
+}
+.left33{
+  left: 33.3%;
+}
+.left66{
+  left: 66.3%;
+}
+.loading{
+  animation-name: spin;
+  animation-duration: .5s;
+  animation-iteration-count: infinite;
+}
+.btn{
+  width: 50%;
+  height: 40px;
+}
+.cashout{
+  position: relative;
+  bottom: 300px;
+}
+.message{
+  text-align: center;
+  font-size: 18px;
+  font-weight: 700;
+  color: white;
+}
+@keyframes spin {
+  0% {transform: rotateY(0deg); }
+  100% {transform: rotateY(360deg);}
 }
 </style>
